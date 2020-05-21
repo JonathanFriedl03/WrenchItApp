1f6fe31b746e9638148637eac550d9823e8c59f6
Jonathan Friedl <jfreetz@yahoo.com>
2020-05-21 18:55:28 -0500

    working on displaying datatables found on datatables.net as a jssection code in the view

---
+++ WrenchIt/wwwroot/js/category.js
@@ -0,0 +1,36 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/category/GetAll",
+            "type": "Get",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            { "data": "displayOrder", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center">
+                                <a href="/category/UpSert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;'>
+                                <i class ='far fa-edit'></i> Edit
+                                &nbsp;
+                                <a onclick=Delete("/category/Delete/${data}") class='btn btn-danger text-white' style='cursor:pointer; width:100px;'>
+                                <i class ='far fa-trash-alt'></i> Delete
+                            </div>
+                                `;
+                }, "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No records found."
+        },
+        "width": "100%"
+    });
+}
